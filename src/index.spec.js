d980ba7125d860ef6106fb48a0cd6427484af223
Justin Perry <just.perry@gmail.com>
2019-10-20 18:24:07 +0100

    Add some initial tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,20 +1,75 @@
 import rxRangeSlider from './index';
 
-describe('rxRangeSlider', () => {
+const createHTMLFixture = content => {
+  const node = document.createElement('div');
+  node.innerHTML = content;
+  return node;
+};
+
+const addToDocument = node => document.body.appendChild(node);
+
+const createSlider = config => {
+  const htmlElement = createHTMLFixture(
+    `
+      <div class="slider" id="slider">
+        <div class="slider__range"></div>
+        <div class="slider__handles">
+          <div tabIndex="1" class="handle slider__handle slider__handle--left"></div>
+          <div tabIndex="2" class="handle slider__handle slider__handle--right"></div>
+        </div>  
+      </div>
+    `
+  );
+  addToDocument(htmlElement);
+
   const sliderNode = document.querySelector('#slider');
-  const config = {
-    bounds: [0, 1000],
-    start: [100, 6000],
-    stepSize: 50,
-    rangeNode: sliderNode.querySelector('.slider__range'),
-    handlesContainerNode: sliderNode.querySelector('.slider__handles'),
-    handleStartNode: sliderNode.querySelector('.slider__handle--left'),
-    handleEndNode: sliderNode.querySelector('.slider__handle--right')
+  const handleStartNode = sliderNode.querySelector('.slider__handle--left');
+  const handleEndNode = sliderNode.querySelector('.slider__handle--right');
+  const rangeNode = sliderNode.querySelector('.slider__range');
+  const handlesContainerNode = sliderNode.querySelector('.slider__handles');
+
+  const myRxRangeSlider = rxRangeSlider({
+    ...config,
+    sliderNode,
+    rangeNode,
+    handlesContainerNode,
+    handleStartNode,
+    handleEndNode
+  });
+
+  return {
+    sliderNode,
+    rangeNode,
+    handlesContainerNode,
+    handleStartNode,
+    handleEndNode,
+    slider: myRxRangeSlider
   };
+};
 
-  const myRxRangeSlider = rxRangeSlider(config);
+describe('rxRangeSlider', () => {
+  const KEY_LEFT = 37;
+  const KEY_RIGHT = 39;
 
-  myRxRangeSlider.subscribe(state => {
-    console.log(state);
+  const arrowKeyRightEvent = new KeyboardEvent('keydown', {
+    bubbles: true,
+    cancelable: true,
+    shiftKey: false,
+    keyCode: KEY_RIGHT
+  });
+
+  it('should increase the start value by the step size', done => {
+    const { slider: myRxRangeSlider, handleStartNode } = createSlider({
+      bounds: [0, 5000],
+      start: [250, 1000],
+      stepSize: 250
+    });
+
+    myRxRangeSlider.subscribe(state => {
+      expect(state.start).toEqual(500);
+      done();
+    });
+
+    handleStartNode.dispatchEvent(arrowKeyRightEvent);
   });
 });
